15f759f90b2f5b81341969d373a916701e65d8e6
angelinagal1 <angelinagaliullina978@gmail.com>
2025-12-15 04:37:18 +0500

    Update vite.config.js
    Signed-off-by: angelinagal1 <angelinagaliullina978@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
   server: {
     open: true,
-    port: 5173,
     host: true
-  },
-  build: {
-    outDir: 'dist',
-    minify: true,
-    reportCompressedSize: true
   }
 })
